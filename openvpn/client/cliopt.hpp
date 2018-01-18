952cccfba692fcafe3446bc133b2d126422ee8eb
Arne Schwabe <arne@rfc2549.org>
2018-01-18 13:11:26 +0100

    Allow to disable route exclusion emulation
    In my tests this does not work with OpenVPN for Android, so fall back to OpenVPN for Android's own implementation for now.

--- openvpn/client/cliopt.hpp
+++ openvpn/client/cliopt.hpp
@@ -62,7 +62,7 @@
 #include <openvpn/transport/gremlin.hpp>
 #endif
 
-#if defined(OPENVPN_PLATFORM_ANDROID)
+#if defined(OPENVPN_PLATFORM_ANDROID) && !defined(NO_ROUTE_EXCLUDE_EMULATION)
 #include <openvpn/client/cliemuexr.hpp>
 #endif
 
@@ -359,9 +359,10 @@ namespace openvpn {
 	    if (config.tun_persist)
 	      tunconf->tun_prop.remote_bypass = true;
 #endif
-#if defined(OPENVPN_PLATFORM_ANDROID)
+#if defined(OPENVPN_PLATFORM_ANDROID) && !defined(NO_ROUTE_EXCLUDE_EMULATION)
 	    // Android VPN API doesn't support excluded routes, so we must emulate them
-	    tunconf->eer_factory.reset(new EmulateExcludeRouteFactoryImpl(false));
+        // OpenVPN for Android does this on its own, so we allow to disable this here
+        tunconf->eer_factory.reset(new EmulateExcludeRouteFactoryImpl(false));
 #endif
 	    if (config.tun_persist)
 	      tunconf->tun_persist.reset(new TunBuilderClient::TunPersist(true, tunconf->retain_sd, config.builder));

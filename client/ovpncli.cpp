9d5115b47532d1799d2de6e7872d886fc2e8be84
Arne Schwabe <arne@rfc2549.org>
2019-08-05 19:19:40 +0200

    Allow overriding reported HW_ADDR and support IV_PLAT_VER

--- client/ovpncli.cpp
+++ client/ovpncli.cpp
@@ -437,6 +437,8 @@ namespace openvpn {
 	std::string tls_cert_profile_override;
 	std::string gui_version;
 	std::string sso_methods;
+	std::string hw_addr_override;
+	std::string platform_version;
 	bool allow_local_lan_access;
 	ProtoContextOptions::Ptr proto_context_options;
 	PeerInfo::Set::Ptr extra_peer_info;
@@ -691,6 +693,8 @@ namespace openvpn {
 	state->allow_local_lan_access = config.allowLocalLanAccess;
 	state->gui_version = config.guiVersion;
 	state->sso_methods = config.ssoMethods;
+	state->platform_version = config.platformVersion;
+	state->hw_addr_override = config.hwAddrOverride;
 	state->alt_proxy = config.altProxy;
 	state->dco = config.dco;
 	state->echo = config.echo;
@@ -965,6 +969,8 @@ namespace openvpn {
       cc.tls_cert_profile_override = state->tls_cert_profile_override;
       cc.gui_version = state->gui_version;
       cc.sso_methods = state->sso_methods;
+      cc.hw_addr_override = state->hw_addr_override;
+      cc.platform_version = state->platform_version;
       cc.extra_peer_info = state->extra_peer_info;
       cc.stop = state->async_stop_local();
       cc.allow_local_lan_access = state->allow_local_lan_access;
